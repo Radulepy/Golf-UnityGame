98ca5edc2b459f429021eea829a9abcdac484c5e
Radu Lepadatu <radulepa@gmail.com>
2020-02-18 23:21:25 +0200

    Update BallControll.cs

--- MGA_GolfGame/Assets/BallControll.cs
+++ MGA_GolfGame/Assets/BallControll.cs
@@ -55,11 +55,11 @@ public class BallControll : MonoBehaviour
 
         //TODOO: DE ALES RI LAS ASTEA 3 DE JOS ORI NUMAI ULTIMA ( COMENTATA )
 
-        //  Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        // MainCam.transform.LookAt(targetPosition);
+          Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+         MainCam.transform.LookAt(targetPosition);
 
 
-        MainCam.GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity;  // ASTA FACE CAMERA SA MEARGA DUPA MINGE (POATE SA ADAUGAM O OPTIUNE)
+       // MainCam.GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity;  // ASTA FACE CAMERA SA MEARGA DUPA MINGE (POATE SA ADAUGAM O OPTIUNE)
 
 
         if (Input.GetKey("space")) // shoots with space key
