dd1b5cac13fb1fcf1a8c1aba489bcf87bb6365fe
Radu Lepadatu <radulepa@gmail.com>
2020-03-14 02:22:04 +0200

    Done

---
+++ MGA_GolfGame/Assets/ButtonHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void GoToMenu()
+    {
+
+        SceneManager.LoadScene("Menu"); 
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("MainScene");
+
+
+    }
+}
