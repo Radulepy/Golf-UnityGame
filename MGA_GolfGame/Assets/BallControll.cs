5930c957d8637d167f9e344879ddb1f5600908ff
Radu Lepadatu <radulepa@gmail.com>
2020-03-14 21:30:02 +0200

    done

--- MGA_GolfGame/Assets/BallControll.cs
+++ MGA_GolfGame/Assets/BallControll.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
--- MGA_GolfGame/Assets/Trigger.cs
+++ MGA_GolfGame/Assets/Trigger.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
-using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 
 public class Trigger : MonoBehaviour
